dc527fd5a849d091e9f31eb79949aab485e25c25
DasBlackfur <dasblackfur@icloud.com>
2025-09-10 11:03:16 +0000

    Update examples

--- example/codec.js
+++ example/codec.js
@@ -1,7 +1,7 @@
 import protobuf from 'k6/x/protobuf';
 
 const data = open("example.json");
-const protoFile = protobuf.load("example/v1/example.proto", "CountryList")
+const protoFile = protobuf.load("example/v1/example.proto", [], "CountryList")
 
 export default function () {
     console.log(protoFile.decode(protoFile.encode(data)))
